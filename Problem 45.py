04f9b842223e07c4823cbffcf6d6a03e7cfb76b4
Noah Grünewald <info@noahgruenewald.de>
2024-09-13 03:24:13 +0200

    Add new problems

--- Problem 45.py
+++ Problem 45.py
@@ -1,6 +1,3 @@
-import math
-
-
 def triangle(n):
     return n * (n + 1) // 2
 
@@ -13,13 +10,22 @@ def hexagonal(n):
     return n * (2 * n - 1)
 
 
+def solve():
+    triangles = set()
+    pentagons = set()
+    hexagons = set()
+    for i in range(1, 100000):
+        triangles.add(triangle(i))
+        pentagons.add(pentagonal(i))
+        hexagons.add(hexagonal(i))
+
+    print(triangles.intersection(pentagons).intersection(hexagons))
+
+
+
 if __name__ == '__main__':
-    for i in range(280, 1000):
-        for j in range(160, 1000):
-            for k in range(140, 1000):
-                if triangle(i) == pentagonal(j) == hexagonal(k):
-                    print(i, j, k)
-                    print(triangle(i), pentagonal(j), hexagonal(k))
+    solve()
+
 
 
 print(triangle(285), pentagonal(165), hexagonal(143))
